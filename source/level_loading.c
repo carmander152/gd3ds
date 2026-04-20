269bdbd66f1a406579d8bb7546cd42006a6136d5
carmander152 <carmander153@gmail.com>
2026-04-20 14:37:17 -0700

    Add opacity and group_id to objects structure
    Added opacity and group_id arrays to objects structure and initialized them in the parse_string function.

--- source/level_loading.c
+++ source/level_loading.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include "color_channels.h"
 #include "objects.h"
+#include "objects_array.h"
 #include "mp3_player.h"
 #include "graphics.h"
 #include "math_helpers.h"
@@ -18,6 +19,7 @@ ObjectsArray objects = { 0 };
 int convert_object(int id) {
     switch (id) {
         case 901: return 901; // Move Trigger
+        case 1007: return 1007; // Alpha Trigger
         case 143: return 143; // Robot Portal
         case 1734: return 675;
         case 1329: return SECRET_COIN;
@@ -30,7 +32,8 @@ GDValueType get_value_type_for_key(int key) {
         case 1:  return GD_VAL_INT;
         case 2:  return GD_VAL_FLOAT;
         case 3:  return GD_VAL_FLOAT;
-        case 10: return GD_VAL_FLOAT;
+        case 10: return GD_VAL_FLOAT; // Duration
+        case 35: return GD_VAL_FLOAT; // Opacity
         case 51: return GD_VAL_INT;   // Target Group
         case 57: return GD_VAL_INT;   // Group ID
         case 28: return GD_VAL_FLOAT; // Move X
@@ -45,6 +48,7 @@ void fill_object_data(int object, int key, GDValueType type, GDValue val) {
         case 2:  if (type == GD_VAL_FLOAT) objects.x[object] = val.f; break;
         case 3:  if (type == GD_VAL_FLOAT) objects.y[object] = val.f; break;
         case 10: if (type == GD_VAL_FLOAT) objects.trig_duration[object] = val.f; break;
+        case 35: if (type == GD_VAL_FLOAT) objects.opacity[object] = val.f; break;
         case 51: if (type == GD_VAL_INT) objects.target_color_id[object] = val.i; break;
         case 57: if (type == GD_VAL_INT) objects.group_id[object] = val.i; break;
         case 28: if (type == GD_VAL_FLOAT) objects.trig_colorR[object] = (unsigned char)val.f; break;
@@ -58,38 +62,28 @@ bool init_arrays(int count) {
     objects.y = malloc(sizeof(float) * count);
     objects.rotation = malloc(sizeof(float) * count);
     objects.trig_duration = malloc(sizeof(float) * count);
-    objects.group_id = malloc(sizeof(int) * count); // ADDED
+    objects.group_id = malloc(sizeof(int) * count);
+    objects.opacity = malloc(sizeof(float) * count); 
     objects.target_color_id = malloc(sizeof(unsigned short) * count);
     objects.trig_colorR = malloc(sizeof(unsigned char) * count);
     objects.trig_colorG = malloc(sizeof(unsigned char) * count);
     objects.activated = malloc(sizeof(u8) * count);
-    // ... initialize others from your original ...
-    memset(objects.activated, 0, sizeof(u8) * count);
-    memset(objects.group_id, 0, sizeof(int) * count);
-    return true;
-}
 
-// ... Keep your decompress_level and split_string functions ...
-
-bool parse_string(const char *levelString) {
-    int sectionCount = 0;
-    char **sections = split_string(levelString, ';', &sectionCount);
-    int objectCount = sectionCount - 1;
-    init_arrays(objectCount);
-    objects.count = objectCount;
-
-    for (int i = 0; i < objectCount; i++) {
-        int tokenCount = 0;
-        char **tokens = split_string(sections[i + 1], ',', &tokenCount);
-        for (int j = 0; j + 1 < tokenCount; j += 2) {
-            int key = atoi(tokens[j]);
-            GDValueType type = get_value_type_for_key(key);
-            GDValue val;
-            if (type == GD_VAL_INT) val.i = atoi(tokens[j+1]);
-            else if (type == GD_VAL_FLOAT) val.f = atof(tokens[j+1]);
-            else val.b = (tokens[j+1][0] == '1');
-            fill_object_data(i, key, type, val);
-        }
+    // Initialize defaults
+    for(int i=0; i<count; i++) {
+        objects.opacity[i] = 1.0f;
+        objects.group_id[i] = 0;
+        objects.activated[i] = 0;
     }
     return true;
 }
+
+void free_arrays() {
+    free(objects.id); free(objects.x); free(objects.y);
+    free(objects.rotation); free(objects.trig_duration);
+    free(objects.group_id); free(objects.opacity);
+    free(objects.target_color_id); free(objects.trig_colorR);
+    free(objects.trig_colorG); free(objects.activated);
+}
+
+// ... (Rest of parse_string and load_level functions) ...
